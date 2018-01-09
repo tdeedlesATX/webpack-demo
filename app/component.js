b901e531780220c34f3e6a10a37bb313b6dc5a39
Tim Dietrich <tim.w.dietrich@gmail.com>
2018-01-09 06:02:34 -0600

    first commit

---
+++ app/component.js
@@ -0,0 +1,7 @@
+export default function createDivElement(text = "Hello world") {
+  const element = document.createElement("div");
+
+  element.innerHTML = text;
+
+  return element;
+};
---
+++ webpack.parts.js
@@ -0,0 +1,25 @@
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    stats: "errors-only",
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+    overlay: {
+      errors: true,
+      warnings: true,
+    },
+  },
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        exclude,
+
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+});
